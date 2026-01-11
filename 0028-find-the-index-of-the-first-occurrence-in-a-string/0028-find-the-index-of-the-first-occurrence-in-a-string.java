5be92300604c6b53c900c5c95368a646143d8f95
Himanshi Yadav <yhimanshi909@gmail.com>
2026-01-11 21:16:55 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.78%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null){
+            return -1;
+        }
+        for(int i=0,j=needle.length();j<=haystack.length();i++,j++){
+            if(haystack.substring(i,j).equals(needle))
+            return i;
+        }
+        return -1;
+    }
+}
